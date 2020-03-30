cbebeccfe053de5350770ab8f1e8783a048b471a
articuly <articuly@gmail.com>
2020-03-30 21:40:25 +0800

    更新account功能，修改密码

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,13 @@ app_name = 'account'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='user_logout'),
-    path('register/', views.register, name='user_register')
+    path('register/', views.register, name='user_register'),
+    # 修改密码
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html',
+                                                                   success_url='/account/password-change-done/'),
+         name='password_change'),
+    path('password-change-done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
+         name='password_change_done'),
+
 ]
--- django_examine/settings.py
+++ django_examine/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = '/blog/'
 
